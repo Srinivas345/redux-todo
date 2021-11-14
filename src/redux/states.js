981b6c7fc756eb957696b8801e47a74bf51c092d
Srinivas345 <srinivas@rapidinnovation.dev>
2021-11-14 22:29:04 +0530

    init

---
+++ src/redux/states.js
@@ -0,0 +1,14 @@
+export let todos =[
+    {
+        id: 1,
+        name: "Cricket",
+    },
+    {
+        id: 2,
+        name: "Chess",
+    },
+    {
+        id: 3,
+        name: "Carroms",
+    },
+]
